aaea33c6989f51788d0b9400d936a961e7b33940
Tad Stiedemann <71119932+q-nmlgb@users.noreply.github.com>
2026-06-28 00:27:24 +0800

    Add files via upload

---
+++ start_server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+JUPYTER_TOKEN=""
+
+NOTEBOOK_DIR="/root"
+
+jupyter labextension disable "@jupyterlab/apputils-extension:announcements"
+
+jupyter-lab \
+    --ip 0.0.0.0 \
+    --port 7860 \
+    --no-browser \
+    --allow-root \
+    --ServerApp.token="" \
+    --ServerApp.tornado_settings="{'headers': {'Content-Security-Policy': 'frame-ancestors *'}}" \
+    --ServerApp.cookie_options="{'SameSite': 'None', 'Secure': True}" \
+    --ServerApp.disable_check_xsrf=True \
+    --LabApp.news_url=None \
+    --LabApp.check_for_updates_class="jupyterlab.NeverCheckForUpdate" \
+    --notebook-dir=$NOTEBOOK_DIR
