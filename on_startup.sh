b4c52e9a62c2d0448213377b85ab24585adffbbc
Tad Stiedemann <71119932+q-nmlgb@users.noreply.github.com>
2026-06-29 15:47:52 +0800

    Add files via upload

---
+++ on_startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /root
+git clone https://github.com/0x4m4/hexstrike-ai.git
+cd hexstrike-ai
+
+# 2. Create virtual environment
+python3 -m venv hexstrike-env
+source hexstrike-env/bin/activate  # Linux/Mac
+# hexstrike-env\Scripts\activate   # Windows
+
+# 3. Install Python dependencies
+pip3 install -r requirements.txt
+python3 hexstrike_server.py --port 8888  >> 123.log  2>&1 &
